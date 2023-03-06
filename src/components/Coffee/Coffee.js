d1f98d09600360f3e65c76028e129e977a1198a6
Erwin Rosales <look4erwin@gmail.com>
2023-03-06 15:33:24 -0800

    Fixed layout of content in cofee cards

--- src/components/Coffee/Coffee.js
+++ src/components/Coffee/Coffee.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 
 const Coffee = ({ coffee, db, deleteCoffee }) => {
   // let { roaster, origin, process, notes } = coffee;
@@ -21,20 +22,27 @@ const Coffee = ({ coffee, db, deleteCoffee }) => {
   // };
 
   return (
-    <div className='grid grid-cols-1 shadow shadow-stone-400 rounded-md'>
-      <div className='py-8 px-9'>
+    <div className='flex flex-col py-8 px-9 justify-between shadow shadow-stone-400 rounded-md'>
+      {/* Coffee Details */}
+
+      <div className='relative'>
         <p className='text-lg font-bold text-black'>{coffee.roaster}</p>
         <p className=' text-gray-600 text-500'>{coffee.origin}</p>
         <p className='mt-4 text-base text-gray-700'>{coffee.notes}</p>
-
+        <button>
+          <EllipsisVerticalIcon className='absolute h-6 top-0 right-0 stroke-gray-400 hover:stroke-black' />
+        </button>
+      </div>
+      {/* Dial buttons */}
+      <div className='justify'>
         <Link
           // to={`coffees/${roaster}-${coffee.origin}`}
-          className='inline-flex items-center justify-center px-4 py-1 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700 mr-4'>
+          className='inline-flex items-center justify-center px-4 py-1 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700 mr-4  hover:-translate-y-1 hover:transition-all'>
           Dials
         </Link>
         <Link
           to={`${coffee.id}/dials`}
-          className='inline-flex items-center justify-center px-4 py-1 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700'>
+          className='inline-flex items-center justify-center px-4 py-1 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700  hover:-translate-y-1 hover:transition-all'>
           Add Dial
         </Link>
       </div>
